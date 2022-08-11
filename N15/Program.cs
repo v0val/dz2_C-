f67c5261ec7bc763632869501e3bcefdd947b20d
valeryO <olennval@mail.ru>
2022-08-11 22:57:46 +0700

    number 15

---
+++ N15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите цифру являющуюся днем недели от 1 до 7");
+int d = Convert.ToInt32(Console.ReadLine());
+if (d == 6 || d == 7)
+{
+Console.WriteLine("день является выходным");
+}
+else
+{
+if (d < 1 || d > 7)
+{
+Console.WriteLine("неверное значения цифры дня недели");  
+}
+else
+{
+Console.WriteLine("день не является выходным");
+}
+
+}
