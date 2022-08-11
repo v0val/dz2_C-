6c21896553193f9bd33f5ffa86df385ddc375b09
valeryO <olennval@mail.ru>
2022-08-11 22:36:19 +0700

    number 13

---
+++ N13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число");
+string a = Console.ReadLine();
+if (Convert.ToInt32(a)<100)
+{
+  Console.WriteLine("третьей цифры нет");  
+}
+else
+{
+Console.WriteLine(a.Substring(2, 1));
+}
